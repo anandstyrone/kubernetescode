d52cb865979c4c381b454f05b4316afac57449d2
Nandu <anandstyrone0316@gmail.com>
2025-11-01 02:25:59 +0000

    changed file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+    return 'Hey Anand,Please subscribe, like, and comment on this video, TY!!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')  # Ensure Flask listens on all interfaces
